ecbd47a1d6a456be01e3435a25d28de1366032c7
thatguy <xarodasimba@gmail.com>
2019-09-12 13:27:55 +0300

    refactored the handleclick in the Die Component. Now its not an arrow function.

--- src/Die.js
+++ src/Die.js
@@ -2,12 +2,19 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+  constructor(props){
+    super(props);
+    this.newHandleClick = this.newHandleClick.bind(this);
+  }
+  newHandleClick(){
+    this.props.handleClick(this.props.idx);
+  }
   render() {
     return (
       <button
         className={"Die"}
         style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={() => this.props.handleClick(this.props.idx)}
+        onClick={this.newHandleClick}
       >
         {this.props.val}
       </button>
